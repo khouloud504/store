ac19fd7e9b0ea583661f8c81e79472a72997478f
khouloud504 <71606832+khouloud504@users.noreply.github.com>
2021-03-22 21:10:39 +0100

    upload website files

---
+++ mapdata.js
@@ -0,0 +1,297 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "450", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#696969",
+    state_hover_color: "#B22222",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: "25",
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    DZA2143: {
+      name: "Adrar",
+      description: "Cas : 8"
+    },
+    DZA2144: {
+      name: "Aïn Témouchent",
+      description: "Cas : 20"
+    },
+    DZA2145: {
+      name: "Oran",
+      color: "#00FFFF",
+      hover_color: "#7FFF00",
+      description: "Cas : 54"
+    },
+    DZA2146: {
+      name: "Sidi Bel Abbès",
+      description: "Cas : 634",
+      hover_color: "#7FFF00",
+      color: "#00FFFF"
+    },
+    DZA2147: {
+      name: "Tlemcen",
+      description: "Cas : 66"
+    },
+    DZA2148: {
+      name: "Béchar",
+      description: "Cas : 87"
+    },
+    DZA2149: {
+      name: "Naâma",
+      description: "Cas : 5"
+    },
+    DZA2150: {
+      name: "Tindouf",
+      description: "Cas : 66"
+    },
+    DZA2163: {
+      name: "Annaba",
+      description: "Cas : 43"
+    },
+    DZA2164: {
+      name: "El Tarf",
+      description: "Cas : 4"
+    },
+    DZA2165: {
+      name: "Jijel",
+      description: "Cas : 11"
+    },
+    DZA2166: {
+      name: "Skikda",
+      description: "Cas :8"
+    },
+    DZA2188: {
+      name: "Illizi",
+      description: "Cas : 22"
+    },
+    DZA2189: {
+      name: "Tamanghasset",
+      description: "Cas : 12"
+    },
+    DZA2190: {
+      name: "El Bayadh",
+      description: "Cas : 4"
+    },
+    DZA2191: {
+      name: "El Oued",
+      description: "Cas :86"
+    },
+    DZA2192: {
+      name: "Ghardaïa",
+      description: "Cas : 33"
+    },
+    DZA2193: {
+      name: "Laghouat",
+      description: "Cas : 54"
+    },
+    DZA2194: {
+      name: "Ouargla",
+      description: "Cas : 221"
+    },
+    DZA2195: {
+      name: "Alger",
+      description: "Cas : 858",
+      color: "#00FFFF",
+      hover_color: "#7FFF00"
+    },
+    DZA2196: {
+      name: "Boumerdès",
+      description: "Cas : 55"
+    },
+    DZA2197: {
+      name: "Tizi Ouzou",
+      description: "Cas : 313",
+      hover_color: "#7FFF00",
+      color: "#00FFFF"
+    },
+    DZA2198: {
+      name: "Tipaza",
+      description: "Cas : 121"
+    },
+    DZA2199: {
+      name: "Aïn Defla",
+      description: "Cas : 143"
+    },
+    DZA2200: {
+      name: "Chlef",
+      description: "Cas : 199",
+      hover_color: "#7FFF00",
+      color: "#00FFFF"
+    },
+    DZA2201: {
+      name: "Mascara",
+      description: "Cas : 103"
+    },
+    DZA2202: {
+      name: "Mostaganem",
+      description: "Cas: 192",
+      hover_color: "#7FFF00",
+      color: "#00FFFF"
+    },
+    DZA2203: {
+      name: "Relizane",
+      description: "Cas : 22"
+    },
+    DZA2204: {
+      name: "Saïda",
+      description: "Cas : 63"
+    },
+    DZA2205: {
+      name: "Tiaret",
+      description: "Cas : 81"
+    },
+    DZA2206: {
+      name: "Tissemsilt",
+      description: "Cas : 032"
+    },
+    DZA2207: {
+      name: "Bordj Bou Arréridj",
+      description: "Cas : 112",
+      hover_color: "#7FFF00",
+      color: "#00FFFF"
+    },
+    DZA2208: {
+      name: "Béjaïa",
+      description: "Cas : 264",
+      hover_color: "#7FFF00",
+      color: "#00FFFF"
+    },
+    DZA2209: {
+      name: "Blida",
+      description: "Cas : 356",
+      color: "#00FFFF",
+      hover_color: "#7FFF00"
+    },
+    DZA2210: {
+      name: "Bouira",
+      description: "Cas : 62"
+    },
+    DZA2211: {
+      name: "Biskra",
+      description: "Cas : 64"
+    },
+    DZA2212: {
+      name: "Djelfa",
+      description: "Cas : 87"
+    },
+    DZA2213: {
+      name: "Médéa",
+      description: "Cas : 170"
+    },
+    DZA2214: {
+      name: "M'Sila",
+      description: "Cas : 19"
+    },
+    DZA2215: {
+      name: "Sétif",
+      description: "Cas : 363",
+      color: "#00FFFF",
+      hover_color: "#7FFF00"
+    },
+    DZA2216: {
+      name: "Batna",
+      description: "Cas : 60"
+    },
+    DZA2217: {
+      name: "Constantine",
+      description: "Cas: 44"
+    },
+    DZA2218: {
+      name: "Guelma",
+      description: "Cas : 32"
+    },
+    DZA2219: {
+      name: "Khenchela",
+      description: "Cas : 24"
+    },
+    DZA2220: {
+      name: "Mila",
+      description: "Cas : 012"
+    },
+    DZA2221: {
+      name: "Oum el Bouaghi",
+      description: "Cas : 75"
+    },
+    DZA2222: {
+      name: "Souk Ahras",
+      description: "Cas : 83"
+    },
+    DZA2223: {
+      name: "Tébessa",
+      description: "Cas :30"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "36.763056",
+      lng: "3.050556",
+      name: "Algiers"
+    }
+  },
+  labels: {},
+  regions: {}
+};
